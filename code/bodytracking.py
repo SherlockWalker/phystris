9f8b233e785ca0ca03e3f8419dfe5a17f7f0be8f
SherlockWalker <140088079+SherlockWalker@users.noreply.github.com>
2026-06-13 11:49:52 +0700

    temporary stash
    last working version

--- code/bodytracking.py
+++ code/bodytracking.py
@@ -4,6 +4,9 @@ from mediapipe import tasks as tasks
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
+from pynput.keyboard import Key
+
+from detection import detectBox;
 
 # Visualisation stuffs, I got off Google's CoLab notebook
 import numpy as np
@@ -26,6 +29,28 @@ def drawLandmarks(rgb_image, detection_result):
 
   return annotated_image
 
+detections = []
+# For some reason CV2 wants your coordinates to be 0 to 1
+
+# LR rotation, LR move
+def createDetections():
+    global detections # I'm highly aware I shouldn't do this (god I wish Python was more similar to C++ features...)
+    detections = [detectBox("LRotate", 0, 0, 0.25, 0.25, Key.left, (0, 255, 0)),
+                 detectBox("RRotate", 0.75, 0, 1, 0.25, Key.right),
+                 detectBox("LMove", 0, 0.75, 0.25, 1, "a"),
+                 detectBox("RMove", 0.75, 0.75, 1, 1, "d")
+                 ]
+# Other than this we need a case to determine if they're on top half of screen or not
+
+def drawDetection(frame, detections):
+    fy, fx, _ = frame.shape
+    for detect in detections: 
+        cv2.rectangle(frame, 
+                      (int(detect.x1 * fx), int(detect.y1 * fy)), 
+                      (int(detect.x2 * fx), int(detect.y2 * fy)),
+                      detect.colour, detect.thickness)
+    return frame;
+
 # Path to model, remember to change later when I have better code structure
 modelPath = "pose_landmarker.task"
 
@@ -46,8 +71,10 @@ def print_result(result, output, timestamp): #For now I don't use timestamp, it 
     if (result.pose_landmarks):
         lastFrame = drawLandmarks(output.numpy_view(), result) #Draw landmark
         lastFrame = cv2.cvtColor(lastFrame, cv2.COLOR_RGB2BGR) # Back to BGR you go:3 for display or something
+        lastFrame = cv2.flip(lastFrame, 1) #Flip camera horizontally
+        lastFrame = drawDetection(lastFrame, detections)
     else:
-        lastFrame = image
+        lastFrame = cv2.flip(image, 1)
 
 # PoseLandmarker instance created
 options = PoseLandmarkerOptions(base_options=BaseOptions(model_asset_path=modelPath),
@@ -57,7 +84,7 @@ options = PoseLandmarkerOptions(base_options=BaseOptions(model_asset_path=modelP
 # Open camera (CV2)
 # I super hate this bit because documentation was really messed up
 # Change to either 0/1/2 depending what camera I have for now. 
-# #My builtin webcam works on some angles and I don't want to activate it
+# My builtin webcam works on some angles and I don't want to activate it so this'll sometimes flip back and forth between 0 and 1
 camera = cv2.VideoCapture(0);
 """
 for i in range(0, 3):
@@ -73,6 +100,16 @@ if (not camera or not camera.isOpened()):
 lastFrame = None;
 
 with PoseLandmarker.create_from_options(options) as landmarker:
+    hasRead, frame = camera.read()
+    if not hasRead:
+        print("Failed to read initial camera frame.")
+        exit()
+        
+    fy, fx, _ = frame.shape
+    cv2.namedWindow("phystris", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("phystris", fx, fy) #Apparently this damn thing is clipping the left side of my camera
+    
+    createDetections();
     while (True):
         hasRead, frame = camera.read()
         if (not hasRead): # Just exit the camera stream entirely
@@ -89,7 +126,9 @@ with PoseLandmarker.create_from_options(options) as landmarker:
 
         #Draw on last frame received, else receive first frame
         if (lastFrame is not None): cv2.imshow("phystris", lastFrame)
-        else: cv2.imshow("phystris", frame)
+        else:
+            lastFrame = drawDetection(frame, detections)
+            cv2.imshow("phystris", lastFrame)
         
         # I forgot to render video lmao
         if (cv2.waitKey(1) & 0xFF == ord('q')):
--- code/detection.py
+++ code/detection.py
@@ -6,22 +6,27 @@ keyboard = Controller();
 About here I kind of figured that if I just fit all this stuff into bodytracking.py I'd have a super bloated file and debugging is hell
 So here's the stuff on where and how I'm going to trigger using body tracking
 Yes, this is basically a macro, I've done macros before and I got banned off a game twice for using them but for different reason
+Typechecking and notes is because I don't want to have to open thousands of files at once lol I'll just rely on python to yell at me
 """
 class detectBox:
-    def __init__(self, name, x1, y1, x2, y2, binding, colour=(255, 0, 0)):
+    def __init__(self, name: str, 
+                       x1: float, y1: float, x2: float, y2: float, 
+                       binding, #I need some special keys so I can't use str
+                       colour=(255, 0, 0), thickness=4):
         self.name = name
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
         self.binding = binding
-        self.colour = colour
+        self.colour = (colour[2], colour[1], colour[0])
+        self.thickness = thickness
         self.collided = False
 
     def trigger(self): keyboard.press(self.binding);
     def untrigger(self): keyboard.release(self.binding);
 
     def collision(self, landmark): #Assuming landmark is of form (x,y) because how else would you store it
-        inside = (self.x1 <= landmark[0] <= self.x2) and (self.y1 <= landmark[0] <= self.y2)
+        inside = (self.x1 <= landmark[0] <= self.x2) and (self.y1 <= landmark[1] <= self.y2)
         if (inside and not self.collided): self.trigger();
         elif (not inside and self.collided): self.untrigger();
